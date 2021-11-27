396a3d8eac78331de09d28f72eb592de2551274a
Mickey D. Luffy <thefalconslayer@gmail.com>
2021-11-27 08:17:08 +0000

    update to redis confg

--- src/main/java/com/mallon/demo/config/RedisConfig.java
+++ src/main/java/com/mallon/demo/config/RedisConfig.java
@@ -3,19 +3,21 @@ package com.mallon.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 @Configuration
 public class RedisConfig {
     @Bean
-    ReactiveRedisOperations<String, Coffee> redisOperations(ReactiveRedisConnectionFactory factory) {
-        Jackson2JsonRedisSerializer<Coffee> serializer = new Jackson2JsonRedisSerializer<>(Coffee.class);
+    JedisConnectionFactory jedisConnectionFactory () {
+        return new JedisConnectionFactory();
+    }
 
-        RedisSerializationContext.RedisSerializationContextBuilder<String, Coffee> builder =
-                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
-
-        RedisSerializationContext<String, Coffee> context = builder.value(serializer).build();
-
-        return new ReactiveRedisTemplate<>(factory, context);
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
     }
 }
