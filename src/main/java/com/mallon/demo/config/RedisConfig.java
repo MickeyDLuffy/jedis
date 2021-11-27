8631af37c02b56ee3f0def39be466f2ee28acecc
Red Emperor <thefalconslayer@gmail.com>
2021-11-27 07:47:50 +0000

    remove redis configuration bean

--- src/main/java/com/mallon/demo/config/RedisConfig.java
+++ src/main/java/com/mallon/demo/config/RedisConfig.java
@@ -7,15 +7,5 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Bean
-    ReactiveRedisOperations<String, Coffee> redisOperations(ReactiveRedisConnectionFactory factory) {
-        Jackson2JsonRedisSerializer<Coffee> serializer = new Jackson2JsonRedisSerializer<>(Coffee.class);
-
-        RedisSerializationContext.RedisSerializationContextBuilder<String, Coffee> builder =
-                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
-
-        RedisSerializationContext<String, Coffee> context = builder.value(serializer).build();
-
-        return new ReactiveRedisTemplate<>(factory, context);
-    }
+  
 }
